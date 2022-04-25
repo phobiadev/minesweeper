0019d91f902873794ade5724c0c6f3e2da8c12fa
cawnsiwup <hugemaleman@gmail.com>
2022-04-25 14:46:57 +0000

    changed number of rows, and altered mine and reset button animation

--- src/App.js
+++ src/App.js
@@ -8,45 +8,41 @@ import Board from "./components/Board.js"
 import { generateBoard } from "./minesweeper"
 
 
-const rows = 8;
-const cols = 8;
-const mines = 10;
+const rows = 18;
+const cols = 18;
+const mines = 30;
 
-export function getTouching(board,row,col) {
+export function getTouching(board, row, col) {
   let touching = 0;
   let possibles = [
-      [row-1,col],
-      [row+1,col],
-      [row,col-1],
-      [row,col+1],
-      [row-1,col-1],
-      [row-1,col+1],
-      [row+1,col-1],
-      [row+1,col+1]
+    [row - 1, col],
+    [row + 1, col],
+    [row, col - 1],
+    [row, col + 1],
+    [row - 1, col - 1],
+    [row - 1, col + 1],
+    [row + 1, col - 1],
+    [row + 1, col + 1]
   ]
   for (let possible of possibles) {
-      try {
-          if (board[possible[0]][possible[1]].isMine) {
-              touching++
-          }
-      } catch(error) {
-          
+    try {
+      if (board[possible[0]][possible[1]].isMine) {
+        touching++
       }
+    } catch (error) {
+
+    }
   }
 
   return touching
 }
 
-function ii(item,array) {
-  return array.map(item => JSON.stringify(item)).includes(JSON.stringify(item))
-}
-
 export default function App() {
-  const [board, setBoard] = useState(generateBoard(rows,cols,mines))
+  const [board, setBoard] = useState(generateBoard(rows, cols, mines))
   const [gameOver, setGameOver] = useState(false);
 
   function reset() {
-    setBoard(generateBoard(rows,cols,mines))
+    setBoard(generateBoard(rows, cols, mines))
     setGameOver(false)
   }
 
@@ -60,41 +56,41 @@ export default function App() {
       }
     }
     setBoard(newBoard)
-    setGameOver(true)
+    setTimeout(() => setGameOver(true), 500);
   }
 
-  function handleCellClick(row,col) {
+  function handleCellClick(row, col) {
     if (!gameOver) {
       let newBoard = JSON.parse(JSON.stringify(board))
       newBoard[row][col].isRevealed = true;
       setBoard(newBoard)
       if (newBoard[row][col].isMine) {
         endGame()
-      } else if (getTouching(board,row,col) === 0) {
+      } else if (getTouching(board, row, col) === 0) {
         let newBoard = JSON.parse(JSON.stringify(board))
-        floodFill(newBoard,row,col)
+        floodFill(newBoard, row, col)
       }
-    }  
+    }
   }
 
-  function floodFill(newBoard,row,col) {
-    
+  function floodFill(newBoard, row, col) {
+
     for (var xoff = -1; xoff <= 1; xoff++) {
       var i = row + xoff;
       if (i < 0 || i >= cols) continue;
-  
+
       for (var yoff = -1; yoff <= 1; yoff++) {
         var j = col + yoff;
         if (j < 0 || j >= rows) continue;
-  
+
         var neighbor = newBoard[i][j];
 
         if (!neighbor.isRevealed) {
           newBoard[i][j].isRevealed = true;
-          if (getTouching(newBoard,i,j) == 0) {
-            floodFill(newBoard,i,j)
+          if (getTouching(newBoard, i, j) == 0) {
+            floodFill(newBoard, i, j)
           }
-          
+
         }
       }
     }
--- src/components/Board.js
+++ src/components/Board.js
@@ -20,32 +20,32 @@ export default function Board(props) {
     return (
         <div className="minesweeper-board">
             {board.map(
-            (row,i) => row.map(
-                (cell,j) => {
-                    let cellInner = <div />
-                    if (cell.isRevealed) {
-                        if (cell.isMine) {
-                            cellInner = <div className="minesweeper-mine" />
-                        } else {
-                            let touching = getTouching(board,i,j)
-                            if (touching > 0) {
-                                cellInner = <div style={{color: touchingColors[touching-1]}}>{touching}</div>
-                            }
-                            
-                        }
-                    }
+                (row, i) => row.map(
+                    (cell, j) => {
+                        let cellInner = <div />
+                        if (cell.isRevealed) {
+                            if (cell.isMine) {
+                                cellInner = <div className="minesweeper-mine" />
+                            } else {
+                                let touching = getTouching(board, i, j)
+                                if (touching > 0) {
+                                    cellInner = <div style={{ color: touchingColors[touching - 1] }}>{touching}</div>
+                                }
 
-                    return (
-                        <div
-                        className={`minesweeper-cell ${!cell.isRevealed ? ((i+j) % 2 === 0 ? "darker-green" : "lighter-green") : "revealed" }`}
-                        onClick={() => handleCellClick(i,j)}  
-                        >
-                        {cellInner}
-                        </div>
-                    )
-                }
-            )
-      )}
+                            }
+                        }
+
+                        return (
+                            <div
+                                className={`minesweeper-cell ${!cell.isRevealed ? ((i + j) % 2 === 0 ? "darker-green" : "lighter-green") : "revealed"}`}
+                                onClick={() => handleCellClick(i, j)}
+                            >
+                                {cellInner}
+                            </div>
+                        )
+                    }
+                )
+            )}
         </div>
     )
 }
